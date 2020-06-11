81a08b40f7b28136282f0ee6c0902bd5a7a83b28
ukarim <13909740+pseudofunctor@users.noreply.github.com>
2020-06-11 00:22:46 -0400

    trying to make webpack works

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const common = require("./webpack.common");
 const merge = require("webpack-merge");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
@@ -34,11 +34,13 @@ module.exports = merge(common, {
   module: {
     rules: [
       {
-        test: /\.scss$/,
+        // test: /\.scss$/,
+        test: /\.css$/,
+
         use: [
           MiniCssExtractPlugin.loader, //3. Extract css into files
           "css-loader", //2. Turns css into commonjs
-          "sass-loader" //1. Turns sass into css
+          // "sass-loader" //1. Turns sass into css
         ]
       }
     ]
