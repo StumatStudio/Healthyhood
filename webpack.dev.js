67f418619d0e71e0fc2591ba6d3833476c1a8a65
ukarim <13909740+pseudofunctor@users.noreply.github.com>
2020-06-10 23:39:35 -0400

    setting up initial version of webpack

---
+++ webpack.dev.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const common = require("./webpack.common");
+const merge = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = merge(common, {
+  mode: "development",
+  entry: './client/index.js',
+  output: {
+    filename: "[name].[contentHash]-bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./client/index.html"
+    })
+  ],
+  module: {
+    rules: [
+      { 
+        test: /\.css$/, 
+        use: ['style-loader', 'css-loader'] 
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader"
+        ]
+      }
+    ]
+  },
+  devServer: {
+    publicPath: '/dist',
+    proxy: {
+      '/api': 'http://localhost:3000'
+    }
+  }
+});
