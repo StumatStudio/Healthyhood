639307662228d59052cfdbef246a5fb90d160180
Matt Severyn <mseveryn@gmail.com>
2020-06-13 13:48:33 -0700

    merging in redux boilerplate to forked master

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,7 @@
 const path = require('path');
-const common = require('./webpack.common');
 const merge = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const common = require('./webpack.common');
 
 module.exports = merge(common, {
   mode: 'development',
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,11 +1,11 @@
 const path = require('path');
-const common = require('./webpack.common');
 const merge = require('webpack-merge');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const common = require('./webpack.common');
 
 module.exports = merge(common, {
   mode: 'production',
@@ -41,8 +41,8 @@ module.exports = merge(common, {
         test: /\.css$/,
 
         use: [
-          MiniCssExtractPlugin.loader, //3. Extract css into files
-          'css-loader', //2. Turns css into commonjs
+          MiniCssExtractPlugin.loader, // 3. Extract css into files
+          'css-loader', // 2. Turns css into commonjs
           // "sass-loader" //1. Turns sass into css
         ],
       },
