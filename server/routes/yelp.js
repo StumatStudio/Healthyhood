29221241be76d8f0eb9b9c8fbccfe9fcdcd4f1d0
smack <smackaholic@gmail.com>
2020-06-15 18:34:22 -0700

    rename yelp business route

--- server/routes/yelp.js
+++ server/routes/yelp.js
@@ -9,10 +9,10 @@ const router = express.Router();
 //    location: street address
 //    latitude: if location not provided
 //    longitude: if location not provided
-router.get('/businesses/search', yelpController.businessSearch, (req, res) => {
+router.get('/business/search', yelpController.businessSearch, (req, res) => {
   // Upon success, res.locals.business will contain an array of businesses
   // returned by the Yelp Business Search API
-  console.log('router.get /businesses/search', res.locals.business);
+  console.log('router.get /business/search', res.locals.business);
 
   // Currently sending the entire blob of data. We should review and consider
   // pruning only to the data we need.
@@ -23,19 +23,15 @@ router.get('/businesses/search', yelpController.businessSearch, (req, res) => {
 //    Returns details about the business given its id
 // Required parameters:
 //    id: business id
-router.get(
-  '/businesses/details',
-  yelpController.businessDetails,
-  (req, res) => {
-    // Upon success, res.locals.businessdetails will contain the business details
-    // returned by the Yelp Business Details API
-    console.log('router.get /businesses/details', res.locals.businessdetails);
+router.get('/business/details', yelpController.businessDetails, (req, res) => {
+  // Upon success, res.locals.businessdetails will contain the business details
+  // returned by the Yelp Business Details API
+  console.log('router.get /business/details', res.locals.businessdetails);
 
-    // Currently sending the entire blob of data. We should review and consider
-    // pruning only to the data we need.
-    res.status(200).send(res.locals.businessdetails);
-  }
-);
+  // Currently sending the entire blob of data. We should review and consider
+  // pruning only to the data we need.
+  res.status(200).send(res.locals.businessdetails);
+});
 
 // Yelp Business Match
 //    Returns list of business that match the search criteria
@@ -45,10 +41,10 @@ router.get(
 //    city: city
 //    sate: state
 //    country: country
-router.get('/businesses/match', yelpController.businessMatch, (req, res) => {
+router.get('/business/match', yelpController.businessMatch, (req, res) => {
   // Upon success, res.locals.business will contain a list of businesses
   // returned by the Yelp Business Match API
-  console.log('router.get /businesses/match', res.locals.business);
+  console.log('router.get /business/match', res.locals.business);
 
   // Currently sending the entire blob of data. We should review and consider
   // pruning only to the data we need.
