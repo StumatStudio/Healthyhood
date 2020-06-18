38e558ba096b4ed5f1068b2de281c685862bc384
smack <smackaholic@gmail.com>
2020-06-18 11:55:31 -0700

    add iqAir route and controller

---
+++ server/routes/iqAir.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const iqAirController = require('../controllers/iqAirController');
+
+const router = express.Router();
+
+router.get('/', iqAirController.getIqAirScore, (req, res) => {
+  // on success, res.locals.iqairscore will contain the IQ Air json blob
+  console.log('router.get / iqairscore', res.locals.iqairscore);
+
+  // currently returning the entire blob of data to the client
+  // should consider reducing this to what is actually needed:
+  // https://api-docs.airvisual.com/?version=latest
+  res.status(200).send(res.locals.iqairscore);
+});
+
+module.exports = router;
