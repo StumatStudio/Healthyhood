ef63bb7164002f3ec1013c1efd078a823742bd82
Matt Severyn <mseveryn@gmail.com>
2020-06-29 22:09:34 -0700

    Implemented Suspend and Map Placeholder

--- client/App.jsx
+++ client/App.jsx
@@ -4,11 +4,9 @@ import NavBar from './components/NavBar/NavBar';
 import ComponentsRouting from './components/ComponentsRouting/ComponentsRouting';
 import Footer from './components/Footer/Footer';
 import BackgroundAnimation from './components/BackgroundAnimation/BackgroundAnimation';
-import MapPlaceholder from './components/common/experimental/MapPlaceholder/MapPlaceholder';
 
 const App = () => (
   <>
-    <MapPlaceholder />
     <LoadScript googleMapsApiKey={process.env.GMAPS_KEY} libraries={['places']}>
       <NavBar />
       <ComponentsRouting />
