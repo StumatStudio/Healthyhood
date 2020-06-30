402ebab8391191e515be9ad148cb46ff6ad5ab75
Matt Severyn <mseveryn@gmail.com>
2020-06-29 18:30:13 -0700

    Placeholder created

--- client/App.jsx
+++ client/App.jsx
@@ -4,9 +4,11 @@ import NavBar from './components/NavBar/NavBar';
 import ComponentsRouting from './components/ComponentsRouting/ComponentsRouting';
 import Footer from './components/Footer/Footer';
 import BackgroundAnimation from './components/BackgroundAnimation/BackgroundAnimation';
+import MapPlaceholder from './components/common/experimental/MapPlaceholder/MapPlaceholder';
 
 const App = () => (
   <>
+    <MapPlaceholder />
     <LoadScript googleMapsApiKey={process.env.GMAPS_KEY} libraries={['places']}>
       <NavBar />
       <ComponentsRouting />
---
+++ client/components/common/experimental/MapPlaceholder/MapPlaceholder.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './MapPlaceholder.css';
+
+const MapPlaceholder = () => (
+  <div className="mapPlace__mapContainer">
+    <div className="mapPlace__img-container mapPlace--loading">
+      <div className="mapPlace__mapSatBar mapPlace--loading" />
+      <div className="mapPlace__fullScreenBox mapPlace--loading" />
+      <div className="mapPlace__streetViewBox mapPlace--loading" />
+      <div className="mapPlace__zoomBar mapPlace--loading" />
+    </div>
+  </div>
+);
+
+export default MapPlaceholder;
